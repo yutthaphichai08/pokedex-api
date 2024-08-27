332dc4809621f33fac8606f2a9dcc9d0a3f91e78
yutthaphichai08 <yutthaphichaikk@gmail.com>
2024-08-27 18:24:59 +0700

    add pagination

--- src/app/components/PokemonList.tsx
+++ src/app/components/PokemonList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import PokemonCard from "./PokemonCard";
 import { Spin } from "antd";
 
@@ -24,12 +24,13 @@ interface PokemonDetails {
 }
 
 export default function PokemonList() {
+  const [allPokemonList, setAllPokemonList] = useState<PokemonDetails[]>([]);
   const [pokemonList, setPokemonList] = useState<PokemonDetails[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [filteredPokemonList, setFilteredPokemonList] = useState<
-    PokemonDetails[]
-  >([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [pageSize] = useState<number>(20);
 
   useEffect(() => {
     fetchPokemonList();
@@ -37,18 +38,19 @@ export default function PokemonList() {
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      const filteredList = pokemonList.filter((pokemon) =>
-        pokemon.name.toLowerCase().includes(searchQuery)
-      );
-      setFilteredPokemonList(filteredList);
+      filterPokemonList();
     }, 300);
 
     return () => clearTimeout(timeoutId);
-  }, [searchQuery, pokemonList]);
+  }, [searchQuery, allPokemonList]);
 
-  const apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`;
+  useEffect(() => {
+    paginatePokemonList();
+  }, [currentPage, pokemonList]);
 
   const fetchPokemonList = async () => {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=300`;
+
     try {
       const response = await fetch(apiUrl);
       const data = await response.json();
@@ -60,18 +62,39 @@ export default function PokemonList() {
       });
 
       const pokemonDetailsList = await Promise.all(pokemonDetailsPromises);
-      setPokemonList(pokemonDetailsList);
-      setFilteredPokemonList(pokemonDetailsList);
+      setAllPokemonList(pokemonDetailsList);
+      console.log("----", pokemonDetailsList);
       setIsLoading(false);
     } catch (error) {
       console.error("Error fetching Pokémon data:", error);
     }
   };
 
+  const filterPokemonList = () => {
+    const filteredList = allPokemonList.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchQuery)
+    );
+    setPokemonList(filteredList);
+    setCurrentPage(1);
+  };
+
+  const paginatePokemonList = () => {
+    setTotalPages(Math.ceil(pokemonList.length / pageSize));
+  };
+
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value.toLowerCase());
   };
 
+  const handlePageChange = (newPage: number) => {
+    if (newPage > 0 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+      setIsLoading(true);
+    }
+  };
+
+  const nextBt = ">";
+  const backBt = "<";
   return (
     <div className="App" style={{ backgroundColor: "#f8f8f8" }}>
       <h1>Pokémon List</h1>
@@ -88,28 +111,43 @@ export default function PokemonList() {
           <Spin size="large" />
         </div>
       ) : (
-        <div className="pokemon-list">
-          {filteredPokemonList.length > 0 ? (
-            filteredPokemonList.map((pokemon) => (
-              <PokemonCard
-                key={pokemon.id}
-                name={pokemon.name}
-                imageUrl={pokemon.sprites.front_default}
-                types={pokemon.types.map((t) => t.type.name)}
-                number={pokemon.id}
-              />
-            ))
-          ) : (
-            <p className="no-results">No Pokémon found.</p>
-          )}
-        </div>
+        <>
+          <div className="pokemon-list">
+            {pokemonList.length > 0 ? (
+              pokemonList
+                .slice((currentPage - 1) * pageSize, currentPage * pageSize)
+                .map((pokemon) => (
+                  <PokemonCard
+                    key={pokemon.id}
+                    name={pokemon.name}
+                    imageUrl={pokemon.sprites.front_default}
+                    types={pokemon.types.map((t) => t.type.name)}
+                    number={pokemon.id}
+                  />
+                ))
+            ) : (
+              <p className="no-results">No Pokémon found.</p>
+            )}
+          </div>
+          <div className="pagination-controls" style={{ marginTop: "20px" }}>
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              {backBt}
+            </button>
+            <span>
+              Page {currentPage} of {totalPages}
+            </span>
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              {nextBt}
+            </button>
+          </div>
+        </>
       )}
-
-      {/* <div style={{ marginTop: "20px" }}>
-        <Button onClick={() => {}} type="primary">
-          Load More
-        </Button>
-      </div> */}
     </div>
   );
 }
